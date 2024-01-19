6fcdd64f4ce4f049dbe6ef406046715606dbd20d
Aryan "LAG" Gupta <aryangupta4feb@gmail.com>
2024-01-19 14:19:37 +0530

    initial

---
+++ detect.py
@@ -0,0 +1,68 @@
+from flask import Flask, request, jsonify
+import cv2
+import numpy as np
+
+app = Flask(__name__)
+
+def highlightFace(net, frame, conf_threshold=0.7):
+    frameOpencvDnn = frame.copy()
+    frameHeight = frameOpencvDnn.shape[0]
+    frameWidth = frameOpencvDnn.shape[1]
+    blob = cv2.dnn.blobFromImage(frameOpencvDnn, 1.0, (300, 300), [104, 117, 123], True, False)
+
+    net.setInput(blob)
+    detections = net.forward()
+    faceBoxes = []
+    for i in range(detections.shape[2]):
+        confidence = detections[0, 0, i, 2]
+        if confidence > conf_threshold:
+            x1 = int(detections[0, 0, i, 3] * frameWidth)
+            y1 = int(detections[0, 0, i, 4] * frameHeight)
+            x2 = int(detections[0, 0, i, 5] * frameWidth)
+            y2 = int(detections[0, 0, i, 6] * frameHeight)
+            faceBoxes.append([x1, y1, x2, y2])
+            cv2.rectangle(frameOpencvDnn, (x1, y1), (x2, y2), (0, 255, 0), int(round(frameHeight / 150)), 8)
+    return frameOpencvDnn, faceBoxes
+
+@app.route('/detect_age_gender', methods=['GET'])
+def detect_age_gender():
+    _, frame = video.read()
+    resultImg, faceBoxes = highlightFace(faceNet, frame)
+    
+    if not faceBoxes:
+        return jsonify({'message': 'No face detected'})
+
+    results = []
+    for faceBox in faceBoxes:
+        face = frame[max(0, faceBox[1]): min(faceBox[3], frame.shape[0]), max(0, faceBox[0]): min(faceBox[2], frame.shape[1])]
+        blob = cv2.dnn.blobFromImage(face, 1.0, (227, 227), (78.4263377603, 87.7689143744, 114.895847746), swapRB=False)
+        
+        genderNet.setInput(blob)
+        genderPreds = genderNet.forward()
+        gender = genderList[genderPreds[0].argmax()]
+        
+        ageNet.setInput(blob)
+        agePreds = ageNet.forward()
+        age = ageList[agePreds[0].argmax()][1:-1] + " years"
+        
+        results.append({'gender': gender, 'age': age})
+
+    return jsonify({'results': results})
+
+if __name__ == '__main__':
+    faceProto = "opencv_face_detector.pbtxt"
+    faceModel = "opencv_face_detector_uint8.pb"
+    ageProto = "age_deploy.prototxt"
+    ageModel = "age_net.caffemodel"
+    genderProto = "gender_deploy.prototxt"
+    genderModel = "gender_net.caffemodel"
+
+    faceNet = cv2.dnn.readNet(faceModel, faceProto)
+    ageNet = cv2.dnn.readNet(ageModel, ageProto)
+    genderNet = cv2.dnn.readNet(genderModel, genderProto)
+    genderList = ['Male', 'Female']
+    ageList = ['(0-2)', '(4-6)', '(8-12)', '(15-20)', '(25-32)', '(38-43)', '(48-53)', '(60-100)']
+
+    video = cv2.VideoCapture(0)
+
+    app.run(debug=True)
